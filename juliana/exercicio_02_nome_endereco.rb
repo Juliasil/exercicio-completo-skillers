41552f192d6f9ef4a6df3d2677c8452898853a3d
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2024-03-09 16:57:37 -0300

    Added solution for exercise 01

---
+++ juliana/exercicio_02_nome_endereco.rb
@@ -0,0 +1,11 @@
+=begin
+# Ler nome, endereço e telefone e imprimi-los.
+nome, endereco , telefone
+escreva(Informe o seu nome)
+leia(nome)
+escreava(Informe seu endereço)
+leia(endereço)
+escreva(Informe seu telefone)
+leia(telefone)
+escreva (" O seu nome enderço e telefone é :"" nome, endereço, telefone "são.")
+=end
