5af9a1afbd727241849e10bfff3e858effad98b1
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2024-03-09 17:45:46 -0300

    Adding the algorithm solution in Portuguese

--- juliana/exercicio_02_nome_endereco.rb
+++ juliana/exercicio_02_nome_endereco.rb
@@ -1,11 +1,15 @@
 =begin
 # Ler nome, endereço e telefone e imprimi-los.
-nome, endereco , telefone
-escreva(Informe o seu nome)
+var
+nome, endereco , telefone: caractere
+
+Inicio
+escreva("Informe o seu nome:")
 leia(nome)
-escreava(Informe seu endereço)
+escreava("Informe seu endereço:")
 leia(endereço)
-escreva(Informe seu telefone)
+escreva("Informe seu telefone")
 leia(telefone)
-escreva (" O seu nome enderço e telefone é :"" nome, endereço, telefone "são.")
+escreva (" O seu nome enderço e telefone são: "+ nome +", " + endereço + , " + telefone + ")
 =end
+
